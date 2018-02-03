1832f1a12f424feaa6cac8ee7299110013d1775a
claudito <1107000044@upci.edu.pe>
2018-02-03 16:45:51 -0500

    inicio clase semana03

---
+++ semana03/bootstrap.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Bootstrap 3</title>
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<!-- Css -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- Jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<!-- JS Bootstrap -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi aliquam nesciunt molestias ex. Nostrum quaerat iste deserunt eaque debitis, vero architecto doloribus asperiores quas explicabo quibusdam id, cupiditate inventore amet.
+</div>
+</div>
+</div>
+
+
+
+</body>
+</html>
